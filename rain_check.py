1d15171ba5762a476e7f8d1f7da066f3f2a9bfcf
marthijn van den brand <mvdbrand@pi.local>
2019-04-19 13:16:17 +0200

    add speak

--- rain_check.py
+++ rain_check.py
@@ -7,8 +7,38 @@ import json
 import time
 import logging
 from logging.handlers import RotatingFileHandler
+from urllib import request as req
+from urllib import parse
+import traceback
 
 logger = logging.getLogger(__name__)
+
+def urlencode(str):
+    return parse.quote(str)
+
+def speak(string):
+    try:
+        url = "http://192.168.1.17:8299/%s"%(urlencode(string))
+        print(url)
+        rq = req.Request(url)
+        req.urlopen(rq)
+    except:
+        traceback.print_exc()
+        pass
+
+def rain_score_text(score):
+    return = 'Dry'
+    if rain_score > 10:
+        return = 'Very light rain'
+    if rain_score > 70:
+        return = 'Light rain'
+    if rain_score > 110:
+        return = 'Persistent rain'
+    if rain_score > 135:
+        return = 'Heavy rain'
+    if rain_score > 165:
+        return = 'Very heavy rain'
+
 
 def check_rain(data):
     rain_url_loc = data['api_url'] + "lat=%s&lon=%s"%(data['latitude'], data['longitude'])
@@ -21,25 +51,14 @@ def check_rain(data):
             try:
                 rainscore, _time = item.split('|')
                 rain_score = max(int(rain_score), int(rainscore))
+                print("rainscore: %s"%rainscore)
             except:
                 logger.error("Failed to parse Buienradar response")
                 pass
 
         if data['pilight_enabled']:
             try:
-                rain_desc = 'Dry'
-                if rain_score > 10:
-                    rain_desc = 'Very light rain'
-                if rain_score > 70:
-                    rain_desc = 'Light rain'
-                if rain_score > 110:
-                    rain_desc = 'Persistent rain'
-                if rain_score > 135:
-                    rain_desc = 'Heavy rain'
-                if rain_score > 165:
-                    rain_desc = 'Very heavy rain'
-
-                os.system("pilight-send -p generic_label -i %s -l '%s, %s'"%(data['pilight_label'], rain_desc, rain_score))
+                os.system("pilight-send -p generic_label -i %s -l '%s, %s'"%(data['pilight_label'], rain_score_text(rain_desc), rain_score))
 
             except:
                 logger.error("Failed to update pilight")
@@ -47,6 +66,9 @@ def check_rain(data):
 
         if rain_score != check_rain.last_rain_score:
             logger.debug("Got new rain score %s"%rain_score)
+            if rain_score > 60:
+                speak("incoming %s detected"%rain_score_text(rain_score))
+
             check_rain.last_rain_score = rain_score
             # post rain score to Olisto
             url = '%s?value=%s'%(data['olisto_connector'], rain_score)
